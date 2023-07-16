ce9e32073288708b107a0e19f447c3ade0f25945
bluepebble25 <bluepebble25@gmail.com>
2023-07-16 23:19:38 +0900

    feat: useIntersect hook 추가 (infinite-scroll)

---
+++ src/InfiniteScroll/hooks/useIntersect.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useCallback } from 'react';
+
+type IntersectHandler = (
+  entry: IntersectionObserverEntry,
+  observer: IntersectionObserver
+) => void;
+
+function useIntersect(
+  onIntersect: IntersectHandler,
+  options?: IntersectionObserverInit
+) {
+  const ref = useRef<HTMLElement>(null); // target 요소를 저장할 ref
+
+  const callback = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      // entries 배열인 이유는 한 observer가 여러 대상을 관찰할 수 있기 때문
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) onIntersect(entry, observer);
+      });
+    },
+    [onIntersect]
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(ref.current);
+
+    return () => observer.disconnect(); // unmount되면 구독 해제
+  }, [ref, options, callback]);
+
+  return ref;
+}
+
+export default useIntersect;
