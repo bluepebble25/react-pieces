bf3895933f212f946bf160f07537696f13e96c83
bluepebble25 <bluepebble25@gmail.com>
2023-07-25 21:20:22 +0900

    feat: 메인페이지에 infinite scroll 링크 추가 및 리스트 디자인 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
 
 function MainPage() {
+  const pageList = [
+    { url: '/search', pageName: 'SearchPage' },
+    { url: '/infinite-scroll', pageName: 'Infinite Scroll' },
+  ];
+
   return (
     <div>
-      <ul style={{ fontSize: '1.8rem' }}>
-        <li>
-          <Link to="/search">SearchPage</Link>
-        </li>
+      <ul
+        style={{ fontSize: '1.8rem', marginTop: '20px', paddingLeft: '40px' }}
+      >
+        {pageList.map((page) => (
+          <li style={{ listStyleType: 'disc' }}>
+            <Link to={page.url}>{page.pageName}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
